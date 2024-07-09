be5bafdf81a50d1ad3e0fe826923020711352759
Eric Capo <eric.capo@hotmail.fr>
2024-07-09 09:27:54 +0200

    Delete js directory

--- js/sidebar.js
+++
@@ -1,4 +0,0 @@
-// Open the sidebar when the hamburger icon is clicked
-document.querySelector('.hamburger').addEventListener('click', function() {
-	document.querySelector('#sidebar').classList.toggle('open');
-});
